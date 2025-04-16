f87fea2d6f32bd5c9051b1dbc6294c0f390effef
ShikoKiev <a.vinogradof@gmail.com>
2025-04-16 12:12:27 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,9 +1,6 @@
 import logging
 import os
 
-from dotenv import load_dotenv
-load_dotenv()
-
 from plugin_manager import PluginManager
 from openai_helper import OpenAIHelper, default_max_tokens, are_functions_available
 from telegram_bot import ChatGPTTelegramBot
